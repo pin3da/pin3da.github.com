b0c4df9bc655110ae715ee0288e005fa45e8c104
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-11-28 14:37:21 -0200

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git co -b master
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+
+hugo
+
+cd public
+git init
+git co -b master
+git remote add origin git@github.com:pin3da/pin3da.github.com.git
+git add -A
+git commit -m 'automatic deploy'
+git push -f origin master
+cd ..
+git co src
